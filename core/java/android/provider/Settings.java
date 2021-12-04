c33ff7f00190775fe9960ef47e046f08686f2c71
Shubham <skpawar1305@gmail.com>
2021-12-04 22:57:16 +0800

    base: Toggle vertical swipes on Ambient display [1/2]
    with single tap to ambient display on, many-a-times there will be accidental
    swipes on display after tap, opening keyguard, prevent this sequence like OxygenOS
    
    * keep it optional
    
    Change-Id: I9c0cf8b7434eca62e5c9e94c64496d6a2c5be5c0
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7144,6 +7144,12 @@ public final class Settings {
         public static final String RIGHT_VERTICAL_BACK_SWIPE_APP_ACTIVITY_ACTION = "right_vertical_back_swipe_app_activity_action";
 
         /**
+         * Enable\Disable swipe up or down gesture on ambient display like OOS
+         * @hide
+         */
+        public static final String DOZE_AMBIENT_SWIPE = "doze_ambient_swipe";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
