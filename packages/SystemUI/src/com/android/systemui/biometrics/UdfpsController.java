a745d67f44413ac80e7813493ef80b6433c4f25d
Dhina17 <dhinalogu@gmail.com>
2023-10-28 11:11:51 +0000

    SystemUI: Simplify udfps animation style selection
    if you want to disable the animation, add none or any string as (which don't have drawables)
    item in the index (which you consider as disabled value)
    of the arrays in the animation package.
    
    Example:
    I added <item>None</item> to the arrays in the index 0
    since I want to use 0 as disabled value.
    
    I don't have any drawables in the name of "None" in the
    udfps animation package.
    It's important to make sure.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I4fe1e34fb3402ed2168fc7d8f15a69fb4084faf3

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -1221,6 +1221,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         cancelAodSendFingerUpAction();
     }
 
+    public boolean isAnimationEnabled() {
+        return mUdfpsAnimation != null && mUdfpsAnimation.isAnimationEnabled();
+    }
+
     /**
      * Callback for fingerUp and fingerDown events.
      */
