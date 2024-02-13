7673c0e985b642a5cf0e1e720e6be364bbf99605
someone5678 <someone5678@users.noreply.github.com>
2024-02-13 16:59:29 +0900

    Gesture nav: Add back menu action
    Change-Id: Idaa6f7496b6babee5735c9eecf41bb8b95f4ace4
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1402,6 +1402,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 17: // Go forward
                 VoltageUtils.sendKeycode(KeyEvent.KEYCODE_FORWARD);
                 break;
+            case 18: // Menu
+                VoltageUtils.sendKeycode(KeyEvent.KEYCODE_MENU);
+                break;
         }
     }
 
