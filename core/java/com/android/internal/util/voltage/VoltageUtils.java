e99548a0a9b9100efd9e44575d213e57510bff81
Aston-Martinn <advaithbhat9@gmail.com>
2024-08-17 17:10:34 +0530

    base: [Utils] Add a method to determine CPU temperature
    Change-Id: I5565ada99b49c7ca2ae734d195ff7b08c7a11d44
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/voltage/VoltageUtils.java
+++ core/java/com/android/internal/util/voltage/VoltageUtils.java
@@ -83,6 +83,12 @@ import android.view.WindowManagerGlobal;
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -147,6 +153,39 @@ public class VoltageUtils {
         // Use boolean to determine celsius or fahrenheit
         return String.valueOf((n - c) % 2 == 0 ? (int) temp :
                 ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
+
+    public static String getCPUTemp(Context context) {
+        String value;
+        if (fileExists(context.getResources().getString(
+            com.android.internal.R.string.config_cpu_temp_path))) {
+               value = readOneLine(context.getResources().getString(
+                  com.android.internal.R.string.config_cpu_temp_path));
+        } else {
+            value = "Error";
+        }
+        return value == "Error" ? "N/A" : String.format("%s", Integer.parseInt(value) / 1000) + "°C";
+    }
+    public static boolean fileExists(String filename) {
+        if (filename == null) {
+            return false;
+        }
+        return new File(filename).exists();
+    }
+    public static String readOneLine(String fname) {
+        BufferedReader br;
+        String line = null;
+        try {
+            br = new BufferedReader(new FileReader(fname), 512);
+            try {
+                line = br.readLine();
+            } finally {
+                br.close();
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return line;
     }
 
     private static final String TAG = "VoltageUtils";
