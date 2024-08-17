cde25e61e6bd14255bb766428fe2176669723dff
Aston-Martinn <advaithbhat9@gmail.com>
2024-08-17 17:30:59 +0530

    SystemUI: Ambient display show battery & cpu temperature [1/2]
    Change-Id: I680cb9057577fa6ae401a5a72b3ae3f7378a9861
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7150,6 +7150,16 @@ public final class Settings {
         public static final String DOZE_AMBIENT_SWIPE = "doze_ambient_swipe";
 
         /**
+         * What to show at the bottom of the Ambient display
+         * 0: Nothing.
+         * 1: Battery Percentage.
+         * 2: Battery Temperature.
+         * 3: Battery Percentage & Temperature Together.
+         * @hide
+         */
+        public static final String AMBIENT_SHOW_SETTINGS = "ambient_show_settings";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
