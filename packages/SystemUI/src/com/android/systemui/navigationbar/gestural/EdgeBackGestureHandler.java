TheParasiteProject/frameworks_base

fixup! base: Add api to take screenshots
* Remove partial screenshot related code from utils and Gesture Nav

Change-Id: I3509b38cbc4190f935227fe4b7c99899ab385712

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1341,7 +1341,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                 CustomUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                CustomUtils.takeScreenshot(true);
+                CustomUtils.takeScreenshot();
                 break;
             case 8: // Notification panel
                 CustomUtils.toggleNotifications();

ij-project/frameworks_base_voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1371,7 +1371,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
                 VoltageUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                VoltageUtils.takeScreenshot(true);
+                VoltageUtils.takeScreenshot();
                 break;
             case 8: // Notification panel
                 VoltageUtils.toggleNotifications();