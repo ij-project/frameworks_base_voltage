Evolution-X-AOSP/frameworks_base

Gesture nav: Add Power menu action
Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: I11cbb003a03b17a981d957cf25aa28138426802d

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1382,6 +1382,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 13: // Switch recent app
                 EvolutionUtils.switchToLastApp(mContext);
                 break;
+            case 14: // Powermenu
+                EvolutionUtils.showPowerMenu();
+                break;
         }
     }
 

ij-project/frameworks_base_voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1391,6 +1391,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 13: // Switch recent app
                 VoltageUtils.switchToLastApp(mContext);
                 break;
+            case 14: // Powermenu
+                VoltageUtils.showPowerMenu();
+                break;
         }
     }
 