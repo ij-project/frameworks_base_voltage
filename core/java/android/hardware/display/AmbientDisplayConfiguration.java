TenX-OS/frameworks_base

base: Fix custom gestures not working if default pulsing is disabled [1/2]
we must be able to trigger ambient if pulse on notification
is disabled but we still want to use tilt, handwave or pocket mode

Change-Id: I05adb0f88b0aaca39d954dcec7c33c41c579b90b
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -53,6 +53,7 @@ public class AmbientDisplayConfiguration {
     public boolean enabled(int user) {
         return pulseOnNotificationEnabled(user)
                 || pulseOnLongPressEnabled(user)
+                || pulseOnCustomDozeEventEnabled(user)
                 || alwaysOnEnabled(user)
                 || wakeLockScreenGestureEnabled(user)
                 || wakeDisplayGestureEnabled(user)
@@ -73,6 +74,14 @@ public class AmbientDisplayConfiguration {
     /** {@hide} */
     public boolean pulseOnNotificationAvailable() {
         return ambientDisplayAvailable();
+    }
+
+    /** {@hide} */
+    private boolean pulseOnCustomDozeEventEnabled(int user) {
+        return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_POCKETMODE_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0)
+                && pulseOnNotificationAvailable();
     }
 
     /** {@hide} */

ij-project/frameworks_base_voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -110,14 +110,17 @@ public class AmbientDisplayConfiguration {
     }
 
     /** @hide */
-    public boolean pulseOnCustomDozeEventEnabled(int user) {
-        return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) != 0)
-                && pulseOnNotificationAvailable();
-    }
-
-    /** @hide */
     public boolean isAmbientTickerEnabled(int user) {
         return boolSettingDefaultOff(Settings.Secure.PULSE_ON_NEW_TRACKS, user);
+    }
+
+    /** {@hide} */
+    private boolean pulseOnCustomDozeEventEnabled(int user) {
+        return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_POCKETMODE_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) != 0)
+                && pulseOnNotificationAvailable();
     }
 
     /** @hide */