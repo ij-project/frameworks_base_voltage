GrapheneOS-Archive/platform_frameworks_base-old

fixup! SystemUI: Require unlocking to use sensitive QS tiles
InternetTileNewImpl is used instead of InternetTile. Keep changes to both for now in case
InternetTileNewImpl usage gets reverted.

packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTileNewImpl.kt
@@ -39,12 +39,14 @@ import com.android.systemui.statusbar.connectivity.AccessPointController
 import com.android.systemui.statusbar.pipeline.shared.ui.binder.InternetTileBinder
 import com.android.systemui.statusbar.pipeline.shared.ui.model.InternetTileModel
 import com.android.systemui.statusbar.pipeline.shared.ui.viewmodel.InternetTileViewModel
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 import javax.inject.Inject
 
 class InternetTileNewImpl
 @Inject
 constructor(
     host: QSHost,
+    keyguardStateController: KeyguardStateController,
     uiEventLogger: QsEventLogger,
     @Background backgroundLooper: Looper,
     @Main private val mainHandler: Handler,
@@ -57,7 +59,7 @@ constructor(
     private val internetDialogFactory: InternetDialogFactory,
     private val accessPointController: AccessPointController,
 ) :
-    QSTileImpl<QSTile.BooleanState>(
+    SecureQSTile<QSTile.BooleanState>(
         host,
         uiEventLogger,
         backgroundLooper,
@@ -66,7 +68,8 @@ constructor(
         metricsLogger,
         statusBarStateController,
         activityStarter,
-        qsLogger
+        qsLogger,
+        keyguardStateController,
     ) {
     private var model: InternetTileModel = viewModel.tileModel.value
 
@@ -84,7 +87,11 @@ constructor(
         return QSTile.BooleanState().also { it.forceExpandIcon = true }
     }
 
-    override fun handleClick(view: View?) {
+    override fun handleClick(view: View?, keyguardShowing: Boolean) {
+        if (checkKeyguard(view, keyguardShowing)) {
+            return
+        }
+
         mainHandler.post {
             internetDialogFactory.create(
                 aboveStatusBar = true,

ij-project/frameworks_base_voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTileNewImpl.kt
@@ -39,12 +39,14 @@ import com.android.systemui.statusbar.connectivity.AccessPointController
 import com.android.systemui.statusbar.pipeline.shared.ui.binder.InternetTileBinder
 import com.android.systemui.statusbar.pipeline.shared.ui.model.InternetTileModel
 import com.android.systemui.statusbar.pipeline.shared.ui.viewmodel.InternetTileViewModel
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 import javax.inject.Inject
 
 class InternetTileNewImpl
 @Inject
 constructor(
     host: QSHost,
+    keyguardStateController: KeyguardStateController,
     uiEventLogger: QsEventLogger,
     @Background backgroundLooper: Looper,
     @Main private val mainHandler: Handler,
@@ -57,7 +59,7 @@ constructor(
     private val internetDialogManager: InternetDialogManager,
     private val accessPointController: AccessPointController,
 ) :
-    QSTileImpl<QSTile.BooleanState>(
+    SecureQSTile<QSTile.BooleanState>(
         host,
         uiEventLogger,
         backgroundLooper,
@@ -66,7 +68,8 @@ constructor(
         metricsLogger,
         statusBarStateController,
         activityStarter,
-        qsLogger
+        qsLogger,
+        keyguardStateController,
     ) {
     private var model: InternetTileModel = viewModel.tileModel.value
 
@@ -84,7 +87,10 @@ constructor(
         return QSTile.BooleanState().also { it.forceExpandIcon = true }
     }
 
-    override fun handleClick(expandable: Expandable?) {
+    override fun handleClick(expandable: Expandable?, keyguardShowing: Boolean) {
+        if (checkKeyguard(expandable, keyguardShowing)) {
+            return
+        }
         mainHandler.post {
             internetDialogManager.create(
                 aboveStatusBar = true,